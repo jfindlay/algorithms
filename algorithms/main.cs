be50da07ff72943de164bdd2749ecb6f08665f71
Justin Findlay <v-jufi@microsoft.com>
2018-11-28 15:42:15 -0800

    Add basic executor, simple insertion sort variants

---
+++ algorithms/main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace algorithms
+{
+    class Algorithms
+    {
+        private static readonly int size = 16;
+        private static readonly int max = 1024;
+        private static int[] A;
+
+        static IEnumerable<int> RandList(Random random) { while (true) { yield return random.Next(max); } }
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            A = RandList(random).Take(size).ToArray();
+
+            // Insertion sort
+            ExecuteAlgorithm<Action<int[]>>(Sort.InsertionSortProc, A, true);
+            ExecuteAlgorithm<Action<int[]>>(Sort.InsertionSortRecr, A, true);
+        }
+
+        static void ExecuteAlgorithm<Alg>(Alg alg, int[] A, bool show_output=false)
+        {
+            // Create A' as a copy of A
+            var Aprime = new int[size];
+            Array.Copy(A, Aprime, A.Length);
+
+            // Execute algorithm on A'
+            if (typeof(Alg) == typeof(Action<int[]>))
+            {
+                ((Delegate)(object)alg).DynamicInvoke(Aprime);
+            }
+            else if (typeof(Alg) == typeof(Func<int[]>))
+            {
+                Aprime = (int[])((Delegate)(object)alg).DynamicInvoke(Aprime);
+            }
+
+            // Display Results
+            if (show_output)  // TODO: replace this with multiple levels of output verbosity: full, diag, perf, none
+            {
+                Console.Write("Before: [" + string.Join(", ", A) + "]\n");
+                Console.Write("After:  [" + string.Join(", ", Aprime) + "]\n");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ algorithms/sort.cs
@@ -0,0 +1,42 @@
+﻿namespace algorithms
+{
+    class Sort
+    {
+        public static void InsertionSortProc(int[] A)
+        {
+            int i = 1, j = 1, x = 0;
+            while (i < A.Length)
+            {
+                x = A[i];
+                j = i - 1;
+                while (j >= 0 && A[j] > x)
+                {
+                    A[j + 1] = A[j];
+                    j--;
+                }
+                A[j + 1] = x;
+                i++;
+            }
+        }
+
+        public static void InsertionSortRecr(int[] A)
+        {
+            void insertion(int[] B, int n)
+            {
+                if (n > 0)
+                {
+                    insertion(B, n - 1);
+                    int x = B[n];
+                    int j = n - 1;
+                    while (j >= 0 && B[j] > x)
+                    {
+                        B[j + 1] = B[j];
+                        j--;
+                    }
+                    B[j + 1] = x;
+                }
+            }
+            insertion(A, A.Length - 1);
+        }
+    }
+}
